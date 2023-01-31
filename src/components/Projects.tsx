55ba607f62dc4040080b7dce6330333a89570b1b
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-01-31 11:51:37 -0800

    finished projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,6 +6,8 @@ import TypescriptIcon from "../../public/typescript.png";
 
 type Props = {};
 
+// scroll doesn't happen once we get past small size 
+
 function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
 
@@ -24,10 +26,32 @@ function Projects({}: Props) {
 
         <div className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll">
           {projects.map((project, index) => (
-            <div className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44">
+            <div className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44" key={index}>
+              <motion.div initial={{y:-300, opacity: 0}} transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, y:0 }} viewport={{once: true}}>
               <Image src={TypescriptIcon} alt="test" className="w-40" />
-              <div>
-                <h4>Case study {index + 1} of 5</h4>
+              </motion.div>
+              <div className="max-w-6xl space-y-10 px-0 md:px-10">
+                <h4 className="text-center text-4xl font-semibold">
+                  <span className="underline decoration-[#f7AB0A]/50">
+                    Case study {index + 1} of {projects.length}:
+                  </span>
+                  UPS clone
+                </h4>
+                <p className="text-center text-lg md:text-left">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                  luctus, mauris non laoreet ullamcorper, lorem elit semper
+                  enim, sed sodales ligula tortor ut urna. Quisque tempus sem id
+                  ex placerat scelerisque. Suspendisse eros quam, aliquet
+                  tristique feugiat sit amet, aliquam mattis nulla. Aenean
+                  pulvinar sodales ipsum a venenatis. Mauris non facilisis leo,
+                  vulputate maximus nulla. Vestibulum a tincidunt tellus, at
+                  condimentum ex. Vivamus in bibendum ligula, id finibus erat.
+                  Mauris sit amet ante nisl. Donec vulputate, nisi ut tempor
+                  lobortis, nisi ipsum rhoncus ligula, nec pharetra ex lectus ut
+                  arcu. Cras mi purus, tristique vitae nibh id, aliquam sagittis
+                  metus.
+                </p>
               </div>
             </div>
           ))}
